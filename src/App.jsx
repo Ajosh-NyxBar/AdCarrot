d6d711de4f8c8166fd47f85a1078389fc30e0727
MuhammadRifkyAkbar <2210631170088@student.unsika.ac.id>
2024-10-04 10:53:00 +0700

    add ABout section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
+import Services from "./components/Services";
 function App() {
   return (
     <div className="overflow-y-hidden bg-orange-50 text-emerald-950 antialiase">
       <Navbar />
       <Hero />
+      <About />
+      <Services />
     </div>
   );
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import { ABOUT } from "../constants";
 
 const About = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="flex min-h-screen flex-col items-center justify-center bg-emerald-50 px-6" id="about">
+      <h2 className="my-8 text-center text-4xl font-semibold tracking-tighter">About Us</h2>
+      <div className="mb-10 flex items-center justify-center gap-4">
+        {ABOUT.profileImages.map((image, index) => (
+            <div key={index} className="w-1/2">
+                <img src={image.src} alt={image.alt} className="w-full rounded-3xl shadow-lg" />
+            </div>
+        ))}
+      </div>
+      <p className="mb-8 max-w-4xl text-center text-xl lg:text-4xl">
+        {ABOUT.aboutUsText}
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
