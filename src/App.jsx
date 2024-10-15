0471f72d5439851736a513bc00eee87cf44d7663
MuhammadRifkyAkbar <2210631170088@student.unsika.ac.id>
2024-10-15 16:24:12 +0700

    init ProjectshowCase

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Services from "./components/Services";
+import ProjectShowcase from "./components/ProjectShowcase";
 function App() {
   return (
     <div className="overflow-y-hidden bg-orange-50 text-emerald-950 antialiase">
@@ -9,6 +10,7 @@ function App() {
       <Hero />
       <About />
       <Services />
+      <ProjectShowcase />
     </div>
   );
 }
